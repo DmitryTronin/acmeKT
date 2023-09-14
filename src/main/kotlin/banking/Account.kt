1260812772a74753a0998b5c91029b7b2f770b20
Cookie Monster <cmonster@sesamestreet.org>
2023-09-14 16:10:50 +0200

    Modify Account class to display success and error messages upon deposit and withdrawal
    * The `deposit` method in the `Account` class was modified to display a success message upon successful deposit and an error message when an error occurs. It now checks if the amount is greater than zero before updating the balance.
    
    * The `withdraw` method in the `Account` class was modified to display a success message upon successful withdrawal and error messages for invalid withdrawal amounts and insufficient balance. It now checks if the amount is greater than zero and if the balance is sufficient before updating the balance.
    
    * A new method `displayBalance` was added to the `Account` class to display the current balance.
    
    Modify Main class to handle user input validation and display appropriate error messages
    
    * The `getUserChoice` function was added to handle user input validation for menu choices. It now checks for empty or non-numeric input and returns the valid choice.
    
    * The `getAmount` function was added to handle user input validation for deposit and withdrawal amounts. It now checks for negative or zero amounts and non-numeric input and returns the valid amount.
    
    * The menu options in the `main` function were updated to utilize the new input validation functions and display appropriate error messages.
    
    * The `displayBalance` method in the `User` class was modified to call the `displayBalance` method in the `Account` class.

--- src/main/kotlin/banking/Account.kt
+++ src/main/kotlin/banking/Account.kt
@@ -4,14 +4,34 @@ class Account {
     var balance: Double = 0.0
 
     fun deposit(amount: Double) {
-        balance += amount
+        if (amount <= 0) {
+            println("Deposit unsuccessful: Invalid value. Amount deposited must be greater than zero.")
+        } else {
+            try {
+                balance += amount
+                println("Deposit successful.")
+            } catch (e: Exception) {
+                println ("Deposit unsuccessful: An error occurred.")
+            }
+        }
     }
 
     fun withdraw(amount: Double) {
-        if (balance >= amount) {
-            balance -= amount
+        if (amount <= 0) {
+            println("Withdrawal unsuccessful: Invalid value. Amount to withdraw must be greater than zero.")
+        } else if (balance < amount) {
+            println("Withdrawal unsuccessful: Insufficient balance.")
         } else {
-            println("Insufficient balance")
+            try {
+                balance -= amount
+                println("Withdrawal successful.")
+            } catch (e: Exception) {
+                println("Withdrawal unsuccessful: An error occurred.")
+            }
         }
     }
+    fun displayBalance() {
+        println("Your current balance is: $balance")
+    }
+    
 }
--- src/main/kotlin/banking/Main.kt
+++ src/main/kotlin/banking/Main.kt
@@ -1,50 +1,91 @@
 package banking
 
-import java.util.Scanner
+import java.util.*
 
 fun main() {
     val scanner = Scanner(System.`in`)
     println("Welcome to banking app")
-    
+
     val validUserNames = listOf("karl", "johan", "sophie", "manuel", "lucas", "emma", "liam", "noah", "mila", "lina")
-    
-    println("Please log in using username:")
-    
-    val name = scanner.nextLine()
-    if(!validUserNames.contains(name)) {
+
+    var name: String
+    while (true) {
+        println("Please log in using username:")
+    name = scanner.nextLine()
+    if (validUserNames.contains(name)) {
+        break
+    } else {
         println("Invalid username")
-        return
-    }       
-    
-    
+    }
+    }
+
+
     val account = Account()
     val user = User(name, account)
-    
+
     var exit = false
-    
+
+    fun getAmount(): Double? {
+        return try {
+            val input = scanner.nextDouble()
+            scanner.nextLine() // consume the newline character
+            if (input <= 0) {
+                println("Negative or zero amounts are not allowed.")
+                null
+            } else {
+                input
+            }
+        } catch (e: InputMismatchException) {
+            println("Invalid input. Please enter a number.")
+            scanner.nextLine() // consume the newline character
+            null
+        }
+    }
+
+    fun getUserChoice(): Int? {
+        return try {
+            val rawInput = scanner.nextLine()
+            if (rawInput.isBlank()) { // check if the input is empty or only contains whitespaces
+                println("Input cannot be empty. Please enter a number.")
+                return null
+            }
+            val choice = rawInput.trim().toInt() // trim the input and convert it to Int
+            choice
+        } catch (e: NumberFormatException) {
+            println("Invalid input. Please enter a number.")
+            null
+        }
+    }
+
     while (!exit) {
         println("\nAvailable operations:")
         println("1: Deposit")
         println("2: Withdraw")
         println("3: Check balance")
         println("4: Exit")
-        
-        when(scanner.nextInt()) {
+
+        val choice = getUserChoice() ?: continue
+        when(choice) {
             1 -> {
                 println("Enter amount to deposit: ")
-                val amount = scanner.nextDouble()
-                scanner.nextLine() // consume the newline character
+                val amount = getAmount() ?: continue
                 user.account.deposit(amount)
             }
             2 -> {
                 println("Enter amount to withdraw: ")
-                val amount = scanner.nextDouble()
-                scanner.nextLine() // consume the newline character
-                user.account.withdraw(amount)
+                val amount = getAmount()
+                if (amount != null) {
+                    user.account.withdraw(amount)
+                } else {
+                    println("Invalid operation. Try again.")
+                }
             }
-            3 -> user.displayBalance()
+            3 -> user.account.displayBalance()
             4 -> exit = true
-            else -> println("Invalid operation. Try again.")
+            else -> {
+                println("Invalid operation. Try again.")
+            }
         }
+
     }
 }
